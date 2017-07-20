d7a13b8be2c7727065cb03bb7e034c12f1f9253b
Jason Fong <jfong27@calpoly.edu>
2017-07-20 14:44:58 -0700

    Able to run own c cmds

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "main.h"
 
 
+
 /**************************************************
  *
  * Function: run_shell
@@ -19,11 +20,10 @@
 void run_shell(){
 
    char **cmd = NULL;
-   int status = 1;
+   int status = 0;
    while(status != -1){
       cmd = read_cline();
-
-      status = -1; 
+      status = execute(cmd);
    }
 
 }
@@ -56,7 +56,65 @@ char **read_cline(){
    return cmd_args;
 
 }
+/**************************************************
+ *
+ * Function: new_process
+ * -------------------
+ *   Forks a new process and execs the 
+ *   desired command. 
+ *
+ *************************************************/
 
+int new_process(char **cmd){
+   int status;
+   pid_t pid; 
+
+   pid = fork();
+   if(pid == 0){
+      execvp(cmd[0],cmd);
+      perror("Exec");
+   }
+   else if(pid > 0){
+      waitpid(pid,&status,0);
+   }
+   else{
+      perror("Fork");
+   }
+
+   return 0;
+}
+
+
+int execute(char **args){
+   int exists = -1;
+   int i;
+   int status = 0;
+
+   if(args[0] != NULL){
+      for(i = 0; i < num_builtins(); i++){
+         if(strcmp(args[0], builtin_str[i])==0){
+            status = (*builtin_func[i])(args);
+            exists = 0;
+         }
+      }
+
+   }
+
+   if(exists == -1)
+      fprintf(stderr, "-myshell: %s: command not found\n", args[0]);
+
+   return status;
+
+
+}
+/**************************************************
+ *
+ * Function: read_line
+ * -------------------
+ *   Reads a line from file pointer input.
+ *   Allocates memory for the line, dynamically.  
+ *
+ *************************************************/
 
 char *read_line(FILE *input){
    int buffer_size = START_SIZE;
@@ -80,6 +138,22 @@ char *read_line(FILE *input){
    *(buffer+pos) = '\0';
    
    return buffer;
+}
+
+int num_builtins(){
+   return sizeof(builtin_str) / sizeof(char*);
+}
+
+int my_cd(char **args){
+   printf("MY CHOOSE DIRECTORY\n");
+
+   return 0;
+}
+
+int my_exit(char **args){
+   printf("MY EXIT\n");
+
+   return 0;
 }
 
 void *checked_malloc(size_t spaces){
--- main.h
+++ main.h
@@ -9,7 +9,20 @@
 
 void run_shell();
 char **read_cline();
+int new_process(char **cmd);
+int execute(char **args);
 char *read_line(FILE *input);
 void *checked_malloc(size_t spaces);
+int num_builtins();
+int my_cd(char **args);
+int my_exit(char **args);
 
+char *builtin_str[] = {
+   "cd",
+   "exit"
+};
+int (*builtin_func[]) (char **) = {
+   &my_cd,
+   &my_exit
+};
 
